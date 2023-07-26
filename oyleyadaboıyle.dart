b904f64510019e446500e29dc908202dbe192aae
hackerFaruk <onuralparslan97@gmail.com>
2023-07-26 11:22:37 +0300

    balbala

---
+++ oyleyadaboıyle.dart
@@ -0,0 +1,10 @@
+main() {
+  int i = 3;
+  if (i == 5) {
+    print("i'nin değeri 5'tir.");
+  } else if (i == 3) {
+    print("i'nin değeri 3'tür.");
+  } else {
+    print("i'nin değeri bilinmiyor");
+  }
+}
